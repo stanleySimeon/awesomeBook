6ea5e6067f3b005be9b1d1ad35a103c38b49c0bb
NatiG <nati.g5684@gmail.com>
2022-02-16 12:34:37 +0300

    Add a function to navigate

--- script.js
+++ script.js
@@ -110,3 +110,34 @@ _add_book.addEventListener('submit', (e) => {
 function displayDate() {
     document.getElementById('date').innerHTML = Date();
 }
+
+const navigate = [
+    {
+        link: 'link_list',
+        target_ID: 'section_list',
+    },
+    {
+        link: 'add_link',
+        target_ID: 'add_section',
+    },
+    {
+        link: 'contact_link',
+        target_ID: 'contact_section',
+    },
+];
+
+navigate.forEach((navigation) => {
+    const link = document.getElementById(navigation.link);
+
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const sections = document.querySelectorAll('main > section');
+        sections.forEach((section) => {
+            section.classList.add('_display_f_');
+        });
+
+        const target = document.getElementById(navigation.target_ID);
+        target.classList.remove('_display_f_');
+    });
+});
